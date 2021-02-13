6d7be4f908e881a41df14a4cbce7b4421f570534
Simon Haugen <smn.hgn@gmail.com>
2021-02-13 12:59:02 +0100

    inject npm_package_version into vite env

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
 import type { Plugin } from 'vite';
 
+let envInjectionFailed = false;
+
 const createPlugin = (): Plugin => {
   return {
     name: 'vite-plugin-package-version',
+    config: (_, env) => {
+      if (env) {
+        return {
+          define: {
+            ['import.meta.env.PACKAGE_VERSION']:
+              process.env.npm_package_version,
+          },
+        };
+      } else {
+        envInjectionFailed = true;
+      }
+    },
+    configResolved(config) {
+      if (envInjectionFailed) {
+        config.logger.warn(
+          `[vite-plugin-package-version] import.meta.env.PACKAGE_VERSION was not injected due ` +
+            `to incompatible vite version (requires vite@^2.0.0-beta.69).`
+        );
+      }
+    },
   };
 };
 
